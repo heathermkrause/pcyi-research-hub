a97a08a1a184b362e110df33480f905f5ca85c18
Pratik <pratik.ganvir@cipher-tech.com>
2013-12-03 23:29:18 +0530

    date issue

--- app/models/document.rb
+++ app/models/document.rb
@@ -6,10 +6,14 @@ class Document < ActiveRecord::Base
   accepts_nested_attributes_for :keyfindings
 
   def self.dump(row,excel_id)
-    doc = new(:report_name => row[0], :author => row[1], :sponsoring_orgnization => row[2], :date_of_report => row[3], :key_recommendations => row[5], :key_ages => row[7], :notes_on_mythodology => row[8], :target_population => row[9], :data_availablity => row[10])
-   	result = doc.save
-   	Keyword.create_keywords(row[6],doc.id) if !row[6].nil? && result
-  	Keyfinding.create(:keyfinding_text => row[4], :document_id => doc.id) if !row[4].nil? && result
+  	begin
+      Date.parse(row[3])
+    rescue
+      row[3] = nil
+    end
+    doc = create(:report_name => row[0], :author => row[1], :sponsoring_orgnization => row[2], :date_of_report => row[3], :key_recommendations => row[5], :key_ages => row[7], :notes_on_mythodology => row[8], :target_population => row[9], :data_availablity => row[10])
+   	Keyword.create_keywords(row[6],doc.id) if !row[6].nil?
+  	Keyfinding.create(:keyfinding_text => row[4], :document_id => doc.id) if !row[4].nil?
   end
 
 end
