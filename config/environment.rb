9b61ee0906332184b54068a1e2406e3349c33f13
Pratik <pratik.ganvir@cipher-tech.com>
2013-12-05 16:20:13 +0530

    host option

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+
 # Initialize the rails application
 DocumentManagementFinal::Application.initialize!
+
+config.action_mailer.default_url_options = { :host => 'http://sheltered-shelf-2154.herokuapp.com' }
