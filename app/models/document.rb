80107aa22f79d0a4f903056870654c86d330bb01
Pratik <pratik.ganvir@cipher-tech.com>
2013-12-06 01:15:57 +0530

    excel issue

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,8 +1,8 @@
 class Document < ActiveRecord::Base
   attr_accessible :author, :data_availablity, :date_of_report, :key_ages, :key_recommendations, :keywords, :notes_on_mythodology, :report_name, :sponsoring_orgnization, :target_population, :user_id, :keyfindings_attributes, :keywords_attributes
   belongs_to :user
-  has_many :keyfindings,:dependent=>:destroy
-  has_many :keywords,:dependent=>:destroy
+  has_many :keyfindings#,:dependent=>:destroy
+  has_many :keywords#,:dependent=>:destroy
   
   accepts_nested_attributes_for :keyfindings
   accepts_nested_attributes_for :keywords
@@ -11,17 +11,14 @@ class Document < ActiveRecord::Base
 
   validates_presence_of :key_ages, :report_name
 
-  validates_length_of :report_name, :maximum => 60, :message=> "less than 60 if you don't mind"
-
-  validates_length_of :report_name, :maximum => 200, :message=> "less than 60 if you don't mind"
-  validates_length_of :author, :maximum => 50, :message => "less than 60 if you don't mind"
-  validates_length_of :data_availablity, :maximum => 50, :message => "less than 60 if you don't mind"
-  validates_length_of :key_ages, :maximum => 50, :message => "less than 60 if you don't mind"
-  validates_length_of :key_recommendations, :maximum => 50, :message => "less than 60 if you don't mind"
-  validates_length_of :notes_on_mythodology, :maximum => 50, :message => "less than 60 if you don't mind"
-  validates_length_of :report_name, :maximum => 50, :message => "less than 60 if you don't mind"
-  validates_length_of :sponsoring_orgnization, :maximum => 50, :message => "less than 60 if you don't mind"
-  validates_length_of :target_population, :maximum => 50, :message => "less than 60 if you don't mind"
+  validates_length_of :report_name, :maximum => 150, :message=> "less than 150 if you don't mind"
+  validates_length_of :author, :maximum => 100, :message => "less than 100 if you don't mind"
+  validates_length_of :data_availablity, :maximum => 50, :message => "less than 50 if you don't mind"
+  validates_length_of :key_ages, :maximum => 50, :message => "less than 50 if you don't mind"
+  validates_length_of :key_recommendations, :maximum => 200, :message => "less than 200 if you don't mind"
+  validates_length_of :notes_on_mythodology, :maximum => 200, :message => "less than 200 if you don't mind"
+  validates_length_of :sponsoring_orgnization, :maximum => 100, :message => "less than 100 if you don't mind"
+  validates_length_of :target_population, :maximum => 50, :message => "less than 50 if you don't mind"
 
 
   def self.dump(row,excel_id,user_id)
@@ -29,17 +26,11 @@ class Document < ActiveRecord::Base
       Date.parse(row[3])
     rescue
       row[3] = nil
-    end
-    puts "creating doc"
-    f = new(:report_name => row[0], :author => row[1], :sponsoring_orgnization => row[2], :date_of_report => row[3], :key_recommendations => row[5], :key_ages => row[7], :notes_on_mythodology => row[8], :target_population => row[9], :data_availablity => row[10], :user_id => user_id)
-    puts "valid doc? "+f.errors.full_messages.join(",")
-    f.save
-    doc = f
-    puts "created doc with "+ doc.id.to_s
+    end 
+    doc = create(:report_name => row[0], :author => row[1], :sponsoring_orgnization => row[2], :date_of_report => row[3], :key_recommendations => row[5], :key_ages => row[7], :notes_on_mythodology => row[8], :target_population => row[9], :data_availablity => row[10], :user_id => user_id)
+    puts "created doc with "+ row[0]
     Keyword.create_keywords(row[6],doc.id) if !row[6].nil?
-    puts "creating finding"
   	Keyfinding.create!(:keyfinding_text => row[4], :document_id => doc.id) if !row[4].nil?
-    puts "finding created"
   end
 
 end
--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -2,8 +2,8 @@ class Keyword < ActiveRecord::Base
   attr_accessible :document_id, :keyword_text
   belongs_to :document
 
-  validates_presence_of :keyword_text
-  validates_length_of :keyword_text, :maximum => 50, :message => "less than 50 if you don't mind"
+  #validates_presence_of :keyword_text
+  #validates_length_of :keyword_text, :maximum => 200, :message => "less than 50 if you don't mind"
   
   def self.create_keywords(keywords,doc_id)
   	separate_keywords = keywords.split(",")
