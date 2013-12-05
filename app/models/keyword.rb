904ce81a4314568b2acdeddf813c03711b341017
Pratik <pratik.ganvir@cipher-tech.com>
2013-12-05 23:45:43 +0530

    excel issue

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -7,7 +7,7 @@ class Keyword < ActiveRecord::Base
   
   def self.create_keywords(keywords,doc_id)
   	separate_keywords = keywords.split(",")
-  	create(separate_keywords.map{|key| {:keyword_text => key, :document_id => doc_id}})
+  	create!(separate_keywords.map{|key| {:keyword_text => key, :document_id => doc_id}})
   end
   
 end
