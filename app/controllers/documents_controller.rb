4573d9aae630dd8599f0faeb32415bcad036f058
ciphitech <ciphitech@gmail.com>
2014-01-09 18:57:34 +0530

    document pdf upload functionality

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -6,7 +6,7 @@ class DocumentsController < ApplicationController
   
   def index
     if current_user.admin
-      @documents = Document.order("created_at ASC").paginate(:page => params[:page])
+      @documents = Document.paginate(:page => params[:page]).order("created_at ASC")
     else
       @documents = current_user.documents.paginate(:page => params[:page])
     end
