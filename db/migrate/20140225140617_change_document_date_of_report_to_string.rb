bc3ef715b47b9b63d76478c27fe0daab6e434e5f
Joshua Majonis <josh@recurbox.com>
2014-02-25 13:33:14 -0500

    Name fix in migration

--- db/migrate/20140225140617_change_document_date_of_report_to_string.rb
+++ db/migrate/20140225140617_change_document_date_of_report_to_string.rb
@@ -1,6 +1,6 @@
 class ChangeDocumentDateOfReportToString < ActiveRecord::Migration
   def change
-    remove_column :documents, :publication_date
+    remove_column :documents, :date_of_report
     add_column :documents, :publication_date, :string
   end
 end
