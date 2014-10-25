0b6e7ed05d3ef76a422ee336b290a6bc8d0e1d77
Jarett Dunn <jare.dunn@hotmail.com>
2014-10-25 11:38:24 -0700

    elance project for Jarett, fixing search, test 1

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -11,11 +11,10 @@ class DocumentsController < ApplicationController
     # Search via Elasticsearch
     if params[:search].present?
       @documents = Document.search(params[:search]).to_a
-
-      # Filter out unmatching tags from initial search results
-      @documents = @documents.keep_if{|doc| doc.tag_names.include?(params[:age_range])} unless params[:age_range].eql?('All ages')
-      @documents = @documents.keep_if{|doc| doc.tag_names.include?(params[:report_type])} unless params[:report_type].eql?('Any report type')
-
+      if !params[:quick]
+        @documents = @documents.keep_if{|doc| doc.tag_names.include?(params[:age_range])} unless params[:age_range].eql?('All ages')
+        @documents = @documents.keep_if{|doc| doc.tag_names.include?(params[:report_type])} unless params[:report_type].eql?('Any report type')
+      end
       @page_title = "Search results"
 
     # Filter by Document tags
